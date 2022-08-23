e82311d3eaa2f64471630141fc50aa37080fdcd6
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2022-08-22 22:04:58 -0300

    upando 1º e 2º aula

---
+++ Aula1_ionic/aula1-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aula1-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
